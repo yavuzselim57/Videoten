42dba14acc8c2c171febba332ea1d7abaaacee5c
yavuzselim57 <yavuzselim5757@yandex.com>
2014-07-11 22:19:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-/* 
-# Çekirdek / Core  : Anveten
-# Ücret / price    : Ücretsiz
-# Yapımcı / Author : Anveten team
-# Copyright (c)    : 2014 Anveten
-*/
+ 
+  $sitebalik = "Videoten Video Scrip" ;
+  echo $sitebaslik;
+ 
+ 
 ?>
